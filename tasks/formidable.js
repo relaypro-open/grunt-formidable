23bc80e840c753e0b043af4519be204b09c3bf5a
Mike Kibbel <mkibbel@gmail.com>
2014-08-13 14:21:40 -0400

    Ready to test initial implementation.

---
+++ tasks/formidable.js
@@ -0,0 +1,64 @@
+'use strict';
+
+module.exports = function(grunt) {
+    var path = require('path'),
+        lang = require('mercenary').lang,
+        extend = lang.extend;
+
+    grunt.task.registerMultiTask(
+        'formidable',
+        'Generate a static site with formidable',
+        function() {
+            var done = this.async(),
+                options = this.options({
+                    // The root of the project, from which other paths will be resolved.
+                    // Defaults to './src' with respect to the grunt base directory
+                    // (usually the same as the grunt file).
+                    root: path.join('src'),
+                    // The build directory into which the site's files will be generated.
+                    // Defaults to '../build' with respect to the root.
+                    build: null,
+                    // The path to the top-level urls module with respect to the root and
+                    // without the '.js' extension. Defaults to 'urls'.
+                    urls: null,
+                    // Template directory globbing patterns for findiing directories in
+                    // addition to the standard '**/templates' pattern. Defaults to [].
+                    templates: null,
+                    // The template rendering engine to use. Defaults to 'swig'.
+                    templating: null,
+                    // Plugins for swig.
+                    swig: {
+                        filters: {},
+                        tags: {},
+                        extensions: {}
+                    },
+                    // Allow overwrite of existing rendered files. When false, an attempted
+                    // overwrite will raise a fatal exception. Defaults to true.
+                    overwrite: null
+                });
+
+            // Instantiate the formidable instance, invoke it and wait for the results.
+            (require('formidable/settings')
+            .load(
+                // The simulated settings.js path.
+                path.join(options.src, 'settings.js'),
+                // The settings.
+                extend({}, options, {
+                    log: {
+                        info: function(message) {
+                            grunt.log.ok(message || '');
+                        },
+                        warn: function(message) {
+                            grunt.log.error(message || '');
+                        },
+                        fail: function(message, code) {
+                            grunt.fail.fatal(message, code);
+                        }
+                    },
+                    verbose: grunt.option('verbose') || false
+                }))()
+            .then(function() {
+                done(true);
+            }));
+        });
+};
